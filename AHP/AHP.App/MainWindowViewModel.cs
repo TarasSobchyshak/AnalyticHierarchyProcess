9345d57e5bedf2bc168facf26f022c18c7e0a564
Taras <Taras@TARAS-PC>
2016-10-04 21:17:55 +0300

    ViewModel added.

---
+++ AHP/AHP.App/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace AHP.App
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        //private int _myProperty;
+
+        //public int MyProperty
+        //{
+        //    get { return _myProperty; }
+        //    set { SetProperty(ref _myProperty, value); }
+        //}
+
+        public MainWindowViewModel()
+        {
+        }
+
+        
+    }
+}
