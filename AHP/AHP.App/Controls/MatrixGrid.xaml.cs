eaf50610c79d2a46504882b40d61cae483f87553
Taras <Taras@TARAS-PC>
2016-10-18 22:52:24 +0300

    Fix

--- AHP/AHP.App/Controls/MatrixGrid.xaml.cs
+++ AHP/AHP.App/Controls/MatrixGrid.xaml.cs
@@ -30,17 +30,4 @@ namespace AHP.App.Controls
         {
         }
     }
-
-    public class MatrixRoDouble2dArrayConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            return (value as Matrix).A;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-    }
 }
